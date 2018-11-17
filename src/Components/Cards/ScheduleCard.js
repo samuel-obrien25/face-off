8b6c3a6cb374e46327a715b3bfb5bb7f21d9c4b4
Sam <samuel.obrien25@gmail.com>
2018-11-16 21:01:22 -0500

    Restructured schedule card & removed old code

--- src/Components/Cards/ScheduleCard.js
+++ src/Components/Cards/ScheduleCard.js
@@ -5,72 +5,26 @@ import AwayTeam from './AwayTeam';
 import HomeTeam from './HomeTeam';
 
 class ScheduleCard extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            isGamePast: false
+        }
+    }
     //TO DO
-      //Convert Date/Time string to a legible date and time
+
 
     render() {
         let jsonDate = new Date(this.props.gameDateTime),
             currentDate = new Date(),
             formattedGameDate = jsonDate.toLocaleDateString() + ' at ' + jsonDate.toLocaleTimeString();
 
-            if (jsonDate < currentDate) {
-                console.log("prev");
-                //Works!
-            }
-
-            console.log(formattedGameDate);
-
-
-
-/*
-        let date = this.props.gameDateTime,
-        splitDate = date.split('-'),
-        month = splitDate[1],
-        dayString = splitDate[2],
-        monthString;
-
-        switch (month) {
-            default:
-                monthString = "test";
-                break;
-            case '01':
-                monthString = "January";
-                break;
-            case '02':
-                monthString = "February";
-                break;
-            case '03':
-                monthString = "March";
-                break;
-            case '04':
-                monthString = "April";
-                break;
-            case '05':
-                monthString = "May";
-                break;
-            case '06':
-                monthString = "June";
-                break;
-            case '07':
-                monthString = "July";
-                break;
-            case '08':
-                monthString = "August";
-                break;
-            case '09':
-                monthString = "September";
-                break;
-            case '10':
-                monthString = "October";
-                break;         
-            case '11':
-                monthString = "November";
-                break;                 
-            case '12':
-                monthString = "December";
-                break;
-        }
-*/
+            if(jsonDate < currentDate) {
+                this.setState = {
+                    isGamePast: true
+                }
+            }   
 
   return (
       <div className="schedule-card card">
