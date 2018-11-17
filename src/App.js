09a5edef285b6a58a1fe153c1514b5d2d838f1d5
Sam <samuel.obrien25@gmail.com>
2018-11-16 21:33:34 -0500

    deploy test

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <Router basename={'/face-off'}>
+      <Router>
       <div className="App">
         <div className="container">
 
