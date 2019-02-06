e9e616ddfa32baac0fff68389c7ed77ecc327980
Sam <samuel.obrien25@gmail.com>
2019-02-06 13:49:35 -0500

    Months change on scroll, fixed padding/underline of Month text

--- src/Components/Containers/MonthContainer.js
+++ src/Components/Containers/MonthContainer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 
 import '../Cards/card.css';
 
@@ -13,97 +13,87 @@ class MonthContainer extends React.Component {
     }
 
     updateH2 = () => {
-        const scheduleList = document.getElementById("card_container_schedule_list"),
-              scheduleListCheckPoint = scheduleList.scrollLeft + 300;
-
-        const MONTHS = {
-                "January":  document.querySelectorAll("[data-month='0']")[0].offsetLeft,
-                "February": document.querySelectorAll("[data-month='1']")[0].offsetLeft,
-                "March":    document.querySelectorAll("[data-month='2']")[0].offsetLeft,
-                "April":    document.querySelectorAll("[data-month='3']")[0].offsetLeft,
-                "October":  document.querySelectorAll("[data-month='9']")[0].offsetLeft,
-                "November": document.querySelectorAll("[data-month='10']")[0].offsetLeft,
-                "December": document.querySelectorAll("[data-month='11']")[0].offsetLeft
+        const SCHEDULE_LIST = document.getElementById("card_container_schedule_list"),
+               MONTHS = {
+                January:  document.querySelectorAll("[data-month='0']")[0].offsetLeft,
+                February: document.querySelectorAll("[data-month='1']")[0].offsetLeft,
+                March:    document.querySelectorAll("[data-month='2']")[0].offsetLeft,
+                April:    document.querySelectorAll("[data-month='3']")[0].offsetLeft,
+                October:  document.querySelectorAll("[data-month='9']")[0].offsetLeft,
+                November: document.querySelectorAll("[data-month='10']")[0].offsetLeft,
+                December: document.querySelectorAll("[data-month='11']")[0].offsetLeft
         };
 
         const setMonthState = (month) => {
                 this.setState({
                     currentMonth: month,
-                })
+                });
         };
+        
+            SCHEDULE_LIST.addEventListener("scroll", () => {
+                let scheduleListCheckPoint = SCHEDULE_LIST.scrollLeft + 300;                
 
-            setTimeout(scheduleList.addEventListener("scroll", () => {
-
-            //If the distance between the first card of month X to the left side of the container 
-            //is less than or equal to the distance the schedule container has been scrolled (plus the size of one card)
+            //If the distance between the first card of month X to the left side of card_container_schedule_list is 
+            //less than or equal to the distance the card_container_schedule_list has been scrolled (plus the size of one card)
             //set state of current month to month X.
 
             //Settimeout so function gets debounced.
-
-            switch (true) {
-                
+            switch(true){
                 case scheduleListCheckPoint > 0 && scheduleListCheckPoint < MONTHS.November:
                     setMonthState("October");
                     break;
                 case scheduleListCheckPoint > MONTHS.October && scheduleListCheckPoint < MONTHS.December:
                     setMonthState("November");
                     break;
+                case scheduleListCheckPoint > MONTHS.November && scheduleListCheckPoint < MONTHS.January:
+                    setMonthState("December");
+                    break;
+                case scheduleListCheckPoint > MONTHS.December && scheduleListCheckPoint < MONTHS.February:
+                    setMonthState("January");
+                    break;
+                case scheduleListCheckPoint > MONTHS.January && scheduleListCheckPoint < MONTHS.March:
+                    setMonthState("February");
+                    break;
+                case scheduleListCheckPoint > MONTHS.February && scheduleListCheckPoint < MONTHS.April:
+                    setMonthState("March");
+                    break;
+                case scheduleListCheckPoint > MONTHS.March:
+                    setMonthState("April");
+                    break;
             }
-        }), 200);
+        });
     }
 
-
     componentDidMount() {
-        //I need to figure out why #card_container_schedule_list returns null. 
+        //I need to figure out why #card_container_schedule_list returns null if I don't use setTimeout. 
         //Until then, setTimeout works around that
         setTimeout(this.updateH2, 1000);
     };
 
+    componentDidUpdate(prevProps, prevState){
+        if(this.state.currentMonth !== prevState.currentMonth){
+            document.getElementById("divisionName").animate([
+                {
+                    opacity: 0,
+                    transform: 'translateY(20px)'
+                },
+                {
+                    opacity: 1,
+                    transform: 'translateY(0px)'
+                }
+            ], {
+                duration: 300,
+                fill: "forwards",
+                easing: "ease-in-out"
+            })
 
-    /*
-function prependMonthSection() {
-    let scheduleCardsList = document.getElementsByClassName("schedule-card"),
-
-        january = document.createElement("H2"),
-        february = document.createElement("H2"),
-        march = document.createElement("H2"),
-        april = document.createElement("H2"),
-        october = document.createElement("H2"),
-        november = document.createElement("H2"),
-        december = document.createElement("H2");
-        
-        january.innerHTML = "January";
-        february.innerHTML = "February";
-        march.innerHTML = "March";
-        april.innerHTML = "April";
-        october.innerHTML = "October";
-        november.innerHTML = "November";
-        december.innerHTML = "December";
-
-    let monthH2s = [january, february, march, april, october, november, december];
-
-        for(let i = 0; i < monthH2s.length; i++){
-            monthH2s[i].className = "month";
         }
-
-    if(scheduleCardsList.length > 60){
-        clearInterval(timer);
-
-        document.querySelectorAll("[data-month='0']")[0].insertAdjacentElement('beforebegin', january);
-        document.querySelectorAll("[data-month='1']")[0].insertAdjacentElement('beforebegin', february);
-        document.querySelectorAll("[data-month='2']")[0].insertAdjacentElement('beforebegin', march);
-        document.querySelectorAll("[data-month='3']")[0].insertAdjacentElement('beforebegin', april);
-        document.querySelectorAll("[data-month='9']")[0].insertAdjacentElement('beforebegin', october);
-        document.querySelectorAll("[data-month='10']")[0].insertAdjacentElement('beforebegin', november);
-        document.querySelectorAll("[data-month='11']")[0].insertAdjacentElement('beforebegin', december);
     }
-} */
-
     render() {
 
         return (
             <div className="month-wrapper">
-                <h2 className="division-name">{this.state.currentMonth}</h2>
+                <h2 id="divisionName" className="division-name">{this.state.currentMonth}</h2>
             </div>
         )
 
