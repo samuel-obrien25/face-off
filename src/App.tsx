65f310d9726f51992560afab73d67100d37372de
Sam <samuel.obrien25@gmail.com>
2019-08-28 15:29:02 -0600

    add typescript and tslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "../node_modules/react-router-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import styled from 'styled-components';
 import Schedule from './Pages/Schedule';
 
