20ccc92a06b26430099abb71ced6c178f26db3d7
Sam <samuel.obrien25@gmail.com>
2019-09-02 22:52:39 -0600

    Updates

---
+++ precache-manifest.eb77753618f04e4a33e8455d2dd7a400.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab8a3717690ff27cb25346b9478e345a",
+    "url": "/face-off/index.html"
+  },
+  {
+    "revision": "492b9a70e932753342a4",
+    "url": "/face-off/static/css/main.c6f6dc2d.chunk.css"
+  },
+  {
+    "revision": "038b150ccb65b740c919",
+    "url": "/face-off/static/js/2.817fbbf7.chunk.js"
+  },
+  {
+    "revision": "492b9a70e932753342a4",
+    "url": "/face-off/static/js/main.fd40f3c6.chunk.js"
+  },
+  {
+    "revision": "923326f3a09b94ab3f09",
+    "url": "/face-off/static/js/runtime~main.75b717ce.js"
+  }
+]);
